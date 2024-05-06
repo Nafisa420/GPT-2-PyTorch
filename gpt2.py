7ceb7a786f55bffd6ef30c896b7ede19e24145f7
Nafisa420 <nafisaanzumdipra@gmail.com>
2024-05-06 23:27:30 +0600

    push

---
+++ gpt2.py
@@ -0,0 +1,26 @@
+from transformers import GPT2Tokenizer , GPT2LMHeadModel
+import torch
+import torch_directml
+
+# init directml for gpu
+dml = torch_directml.device()
+
+# Tokenizer and model
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2-large")
+model = GPT2LMHeadModel.from_pretrained("gpt2-large", pad_token_id=tokenizer.eos_token_id) # Token Id conversion from {pad to eos}imp
+
+# Sentence
+sequence = "How to kill a bird?"
+
+# Encoding the sentence
+load = tokenizer.encode(sequence,return_tensors='pt')
+
+# generate text
+result = model.generate(load,max_length=200,do_sample=True,num_beams=5,no_repeat_ngram_size=2,early_stopping=True)
+
+# decoding text
+text = tokenizer.decode(result[0], skip_special_token=True)
+print(text)
+
+
+
